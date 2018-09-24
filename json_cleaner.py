f85f52ee3b85db46bfcf5358f374a0a069e7573a
artlands <lijie0416@gmail.com>
2018-09-24 17:49:32 -0500

    enhance funtion

--- json_cleaner.py
+++ json_cleaner.py
@@ -4,6 +4,10 @@
 import sys
 import os
 import json
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import preprocessor as p
+p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.HASHTAG, p.OPT.RESERVED, p.OPT.EMOJI, p.OPT.SMILEY, p.OPT.NUMBER)
 
 class JsonCleaner:
 
@@ -16,27 +20,74 @@ class JsonCleaner:
         cleaner.clean()
 
     def clean(self):
-        print "Cleaning %s, only keep attribute(s): %s" % (os.path.join(self.working_dir, self.file_base_name + self.file_ext), self.keep_attr)
+        strInProcess = "Cleaning %s, only keep attribute(s): %s" % (os.path.join(self.working_dir, self.file_base_name + self.file_ext), self.keep_attr)
+        print (strInProcess)
 
         out_file = self.get_new_file()
+        stop_words = set(stopwords.words('english'))
         for line in self.in_file:
             data = json.loads(line)
             clean_data = {}
-            for attr in self.keep_attr:
-                if attr in data:
-                    clean_data[attr] = data[attr]
-            out_file.write('%s\n' % json.dumps(clean_data))
+            if 'text' in data and 'place' in data and data['place'] != None and self.check_place(data['place']['full_name']): # only keep tweets with valid text and place
+                for attr in self.keep_attr:
+                    if attr == 'text':
+                        clean_data['text'] = data['text']
+                        basic_clean = p.clean(data['text'])         #basic clean
+                        word_tokens = word_tokenize(basic_clean)       #tokenize strings
+                        filtered_sentence = []
+                        for w in word_tokens:
+                            if w not in stop_words:
+                                filtered_sentence.append(w)         #delete stop words
+                        clean_data['tokens'] = word_tokens
+                    if attr == 'place':
+                        clean_data['place'] = self.trans_place(data['place']['full_name'])    #simplify place info
+                    else:
+                        clean_data[attr] = data[attr]
+                out_file.write('%s\n' % json.dumps(clean_data))
 
         out_file.close()
-
-        print "Created a cleaned json!"
+        strFinished = "Created a cleaned json!"
+        print (strFinished)
 
     def get_new_file(self):
         """return a new file object ready to write to """
         new_file_name = "%s_cleaned%s" % (self.file_base_name, self.file_ext)
         new_file_path = os.path.join(self.working_dir, new_file_name)
-        print "creating file %s" % (new_file_path)
+        strCreatFile = "creating file %s" % (new_file_path)
+        print (strCreatFile)
         return open(new_file_path, "w")
+
+    def check_place(self, place):
+        """check if place is in USA"""
+        validPlace = ["AL","AK","AZ","AR","CA","CO" "CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI",
+            "MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK",
+            "OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY", "USA"]
+        strSplited = place.split(", ")
+        if len(strSplited) == 2 and strSplited[1] in validPlace:
+            return True
+        else:
+            return False
+
+    def trans_place(self, place):
+        """nomralize state name, eg Iowa, USA -> IA; San Francisco, CA ->"""
+        states = {
+            'Alabama': "AL","Alaska" : "AK","Arizona" : "AZ","Arkansas" : "AR","California" : "CA","Colorado" : "CO","Connecticut" : "CT",
+            "Delaware" : "DE","Florida" : "FL","Georgia" : "GA","Hawaii" : "HI","Idaho" : "ID","Illinois" : "IL","Indiana" : "IN","Iowa" : "IA",
+            "Kansas" : "KS","Kentucky" : "KY","Louisiana" : "LA","Maine" : "ME","Maryland" : "MD","Massachusetts" : "MA","Michigan" : "MI",
+            "Minnesota" : "MN","Mississippi" : "MS","Missouri" : "MO","Montana" : "MT","Nebraska" : "NE","Nevada" : "NV","New Hampshire" : "NH",
+            "New Jersey" : "NJ","New Mexico" : "NM","New York" : "NY","North Carolina" : "NC","North Dakota" : "ND","Ohio" : "OH","Oklahoma" : "OK",
+            "Oregon" : "OR","Pennsylvania" : "PA","Rhode Island" : "RI","South Carolina" : "SC","South Dakota" : "SD","Tennessee" : "TN",
+            "Texas" : "TX","Utah" : "UT","Vermont" : "VT","Virginia" : "VA","Washington" : "WA","West Virginia" : "WV","Wisconsin" : "WI","Wyoming" : "WY"
+        }
+        strSplited = place.split(", ")
+        if strSplited[1] == 'USA':
+            if strSplited[0] != "District of Columbia":
+                newplace = states[strSplited[0]]
+            else:
+                newplace = 'MD'
+        else:
+            newplace = strSplited[1]
+        return newplace
 
     def parse_args(self, argv):
         """parse args and set up instance variables"""
@@ -51,7 +102,7 @@ class JsonCleaner:
             self.working_dir = os.getcwd()
             self.file_base_name, self.file_ext = os.path.splitext(self.file_name)
         except:
-            print self.usage()
+            print (self.usage())
             sys.exit(1)
 
     def usage(self):
