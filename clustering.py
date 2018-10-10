a3990c714790217e8bd6ae1378a0284057a18f77
Artlands <artlands@users.noreply.github.com>
2018-10-09 21:07:09 -0500

    hierarchy document clustering

--- clustering.py
+++ clustering.py
@@ -23,12 +23,16 @@ stemmer = SnowballStemmer("english")
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.cluster import KMeans
-from sklearn.externals import joblib
+# from sklearn.cluster import KMeans
+# from sklearn.externals import joblib
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# from sklearn.manifold import MDS
 
-
-# stopwords = nltk.corpus.stopwords.words('english')
+from scipy.cluster.hierarchy import ward, dendrogram
 
 class Cluster:
 
@@ -61,92 +65,52 @@ class Cluster:
         stems = [stemmer.stem(t) for t in filtered_tokens]
         return stems
 
-    def tokenize_only(self, raw_text):
-        # remove unicode
-        text = "".join([x for x in raw_text if ord(x) < 128])
-        # use preprocessor to clean the text
-        cleaned_text = p.clean(text)
-        # convert to lower case and split
-        words = cleaned_text.lower().split()
-        # remove stopwords
-        stopword_set = set(stopwords.words("english"))
-        meaningful_words = [w for w in words if w not in stopword_set]
-        # join the cleaned words in a list
-        cleaned_word_list = " ".join(meaningful_words)
-
-        tokens = [word for sent in nltk.sent_tokenize(cleaned_word_list) for word in nltk.word_tokenize(sent)]
-        filtered_tokens = []
-        for token in tokens:
-            if re.search('[a-zA-Z]', token):
-                filtered_tokens.append(token)
-        return filtered_tokens
-
     def cluster(self):
-        print (f"Reading the data...")
         start = time.time()
 
         # Read data
+        print (f"Reading the data...")
         for line in self.in_file:
             data = json.loads(line)
             documents = data['documents']
             texts = data['text']
 
-        print (f"Tokenizing and stemming vocabularies...")
-        totalvocab_stemmed = []
-        totalvocal_tokenized = []
-
-        for i in texts:
-            allwords_stemmed = self.tokenize_and_stem(i)
-            totalvocab_stemmed.extend(allwords_stemmed)
-
-            allwords_tokenized = self.tokenize_only(i)
-            totalvocal_tokenized.extend(allwords_tokenized)
-
-        print (f"Creating a DataFrame for tokenized vocab and stemmed vocab...")
-        vocab_frame = pd.DataFrame({'words': totalvocal_tokenized}, index = totalvocab_stemmed)
-        print(f"There are {str(vocab_frame.shape[0])} items in vocab_frame.")
-        # Return the first 10 rows
-        # print(vocab_frame.head(n = 10))
-
         # Convert a collection of raw documents to a matrix of TF-IDF features.
         print (f'Creating tfidf matrix...')
         tfidf_vectorizer = TfidfVectorizer(max_df = 0.90, max_features = 100000, min_df = 0.10, use_idf=True, tokenizer = self.tokenize_and_stem, ngram_range=(1, 3))
+        # Apply tfidf normalization to a sparse matrix of occurrence counts
         tfidf_matrix = tfidf_vectorizer.fit_transform(texts)
 
         print(f'Tfidf matrix shape: {tfidf_matrix.shape}')
         terms = tfidf_vectorizer.get_feature_names()
         dist = 1 - cosine_similarity(tfidf_matrix)
 
-        #K-means Clustering
-        print (f'K-means Clustering...')
-        num_clusters = 5
-        km = KMeans(n_clusters = num_clusters)
-        km.fit(tfidf_matrix)
+        #hierarchy document clustering------------------------------------------
+        #Ward clustering is an agglomerative clustering method, meaning that at each stage,
+        #the pair of clusters with minimum between-cluster distance are merged. Used the precomputed
+        # cosine distance matrix (dist) to calclate a linkage_matrix, which then plot as a dendrogram.
 
-        joblib.dump(km, 'doc_cluster.pkl')
-        clusters = km.labels_.tolist()
+        print (f'Hierarchy document clustering...')
+        linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
 
-        tweets = {'documents': documents, 'texts': texts, 'cluster': clusters}
-        frame = pd.DataFrame(tweets, index = [clusters], columns = ['documents', 'cluster'])
-        # Write dataframe to a json file
-        out_file = self.get_new_file()
-        out_file.write('%s\n' % json.dumps(frame.to_json(orient='records')))
-        out_file.close()
-        # frame['cluster'].value_counts()
-        #
-        # grouped = frame['documents'].groupby(frame['cluster'])
-        # grouped.mean()
-        #
+        fig, ax = plt.subplots(figsize=(15, 20)) # set size
+        ax = dendrogram(linkage_matrix, orientation="right", labels=documents);
+
+        plt.tick_params(\
+            axis= 'x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom= False,      # ticks along the bottom edge are off
+            top= False,         # ticks along the top edge are off
+            labelbottom= False)
+
+        plt.tight_layout() #show plot with tight layout
+
+        #Save figure
+        plt.savefig('ward_clusters.png', dpi=200) #save figure as ward_clusters
+        #-----------------------------------------------------------------------
+
         end = time.time()
         print (f"It takes {end-start}s")
-
-    def get_new_file(self):
-        """return a new file object ready to write to """
-        new_file_name = f"data/dataframes{self.file_ext}"
-        new_file_path = os.path.join(self.working_dir, new_file_name)
-        print (f"Creating file {new_file_path}")
-        return open(new_file_path, "w")
-
 
     def parse_args(self,argv):
         """parse args and set up instance variables"""
